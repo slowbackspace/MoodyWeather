98e26656310d6844bf1bcc4c187553eefa295557
slowbackspace <slowbackspace@gmail.com>
2018-11-26 01:27:54 +0100

    use getCurrentPosition instead of watchPosition

--- src/helpers/geolocation.js
+++ src/helpers/geolocation.js
@@ -1,9 +1,15 @@
 export const getCurrentPosition = () => {
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+
   return new Promise((resolve, reject) => {
     if (!navigator.geolocation) {
       reject("Geolocation is not supported");
     } else {
-      navigator.geolocation.watchPosition(
+      navigator.geolocation.getCurrentPosition(
         position => {
           resolve({
             latitude: position.coords.latitude,
@@ -12,7 +18,8 @@ export const getCurrentPosition = () => {
         },
         err => {
           reject(`Can't get current location: ${err.message}`);
-        }
+        },
+        options
       );
     }
   });
